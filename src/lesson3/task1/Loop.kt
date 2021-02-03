7cb242da7e6d1ac82accf07092d575902db2e1e0
Roman Yarullin <roman.yarullin@zyfra.com>
2021-02-03 19:22:25 +0300

    Сделана 2 простые задачи к уроку 3

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -67,15 +67,36 @@ fun digitCountInNumber(n: Int, m: Int): Int =
  *
  * Использовать операции со строками в этой задаче запрещается.
  */
-fun digitNumber(n: Int): Int = TODO()
-
+fun digitNumber(n: Int): Int {
+    if (n == 0)
+        return 1
+    var count = 0
+    var m = n
+    while (m != 0) {
+        count += 1
+        m /= 10
+    }
+    return count
+}
 /**
  * Простая
  *
  * Найти число Фибоначчи из ряда 1, 1, 2, 3, 5, 8, 13, 21, ... с номером n.
  * Ряд Фибоначчи определён следующим образом: fib(1) = 1, fib(2) = 1, fib(n+2) = fib(n) + fib(n+1)
  */
-fun fib(n: Int): Int = TODO()
+fun fib(n: Int): Int {
+    if (n == 1 || n == 2)
+        return 1
+    var res1 = 1
+    var res2 = 1
+    var res = 0
+    for (i in 3..n) {
+        res = (res2 + res1)
+        res1 = res2
+        res2 = res
+    }
+    return res
+}
 
 /**
  * Простая
